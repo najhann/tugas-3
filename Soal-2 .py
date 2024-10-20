ee7ebec4ab1e363acbfd1ebc19e15d992c32e2e3
najhann <162579224+najhann@users.noreply.github.com>
2024-10-19 20:36:17 -0700

    Update Soal-2 .py

--- Soal-2 .py
+++ Soal-2 .py
@@ -1,6 +1,6 @@
-def print_pattern(n):
-    for i in range(n):
-        print(" " * (i * 2) + "* " * (n - i))
-
-n = 8
-print_pattern(n)
+for i in range(8):
+    for j in range(i):
+        print(" ",end="")
+    for k in range(8-i):
+        print("*",end="")
+    print()
